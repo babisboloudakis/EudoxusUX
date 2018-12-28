bcb544ef2dee540f8c2b673db873c822ff3995e7
Babis Boloudakis <34814013+babisboloudakis@users.noreply.github.com>
2018-12-28 23:39:27 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    // Config like with options required in most files
+    $server = "localhost:3307";
+    $db_user = "root";
+    $db_password = "";
+    $mysqli = new mysqli($server,$db_user);
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+
+
+
+<html>
+    <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    </head>
+    <body>
+    <?php 
+        include 'config.php';
+        // Create database
+        echo '<h1 class="display-1 text-primary">Hello world!</ h1>';
+        echo '<button type="button" class="btn btn-danger m-5">Danger</button>';
+
+    ?>
+    </body>
+</html>
+
+
+
