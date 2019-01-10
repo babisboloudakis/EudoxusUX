2de7783917c499ef3adcf36778e50317e5f76c08
Babis Boloudakis <34814013+babisboloudakis@users.noreply.github.com>
2019-01-10 23:30:51 +0200

    More dummy data

--- db.php
+++ db.php
@@ -4,6 +4,7 @@
     if ( $mysqli->query("CREATE TABLE `users` (
     `id` int(4) NOT NULL auto_increment,
     `username` varchar(65) NOT NULL default '',
+    `email` varchar(65) not NULL default '',
     `password` varchar(65) NOT NULL default '',
     `telephone` varchar(65) default'',
     `type` varchar(65) default 'student',
@@ -16,28 +17,23 @@
 
     if ( $mysqli->query("CREATE TABLE `students` (
     `id` int(4) NOT NULL auto_increment,
-    `username` varchar(65) NOT NULL default '',
-    `password` varchar(65) NOT NULL default '',
-    `telephone` varchar(65) default'',
-    `type` varchar(65) default 'student',
+    `university` varchar(65) NOT NULL default '',
+    `semester` varchar(65) NOT NULL default '',
     PRIMARY KEY (`id`)
     )" ) ) {
-        echo "Users Table created!";
+        echo "Students Table created!";
     } else {
-        echo "Users Table creation failed!";
+        echo "Students Table creation failed!";
     }
 
     if ( $mysqli->query("CREATE TABLE `publishers` (
     `id` int(4) NOT NULL auto_increment,
-    `username` varchar(65) NOT NULL default '',
-    `password` varchar(65) NOT NULL default '',
-    `telephone` varchar(65) default'',
-    `type` varchar(65) default 'student',
+    `vat` varchar(65) NOT NULL default '',
     PRIMARY KEY (`id`)
     )" ) ) {
-        echo "Users Table created!";
+        echo "Publishers Table created!";
     } else {
-        echo "Users Table creation failed!";
+        echo "Publishers Table creation failed!";
     }
 
     // Create books table
@@ -46,13 +42,35 @@
     `name` varchar(65) NOT NULL default '',
     `author` varchar(65) NOT NULL default '',
     `image` varchar(65) default'',
-    `description` varchar(65) default'',
+    `description` varchar(600) default'',
     `category` varchar(65),
     PRIMARY KEY (`id`)
     )" ) ) {
         echo "Books Table created!";
     } else {
         echo "Books Table creation failed!";
+    }
+
+    if ( $mysqli->query("CREATE TABLE `books_universities` (
+    `id` int(4) NOT NULL,
+    `university` varchar(65) NOT NULL default '',
+    PRIMARY KEY (`id`,`university`)
+    )" ) ) {
+        echo "Books university Table created!";
+    } else {
+        echo "Books Table university creation failed!";
+    }
+
+    if ( $mysqli->query("CREATE TABLE `announcements` (
+    `id` int(4) NOT NULL auto_increment,
+    `title` varchar(65) NOT NULL default '',
+    `text` varchar(600) NOT NULL default '',
+    `img` varchar(65) NOT NULL default '',
+    PRIMARY KEY (`id`)
+    )" ) ) {
+        echo "Announcement Table created!";
+    } else {
+        echo "Announcement creation failed!";
     }
 
     // echo "Now creating dummy data..";
@@ -62,29 +80,81 @@
     //     echo "Failed to insert dummy data";
     // }
 
-    if ( $mysqli->query("INSERT INTO books (name,author) VALUES ('EAM','Roussou')") ) {
-        echo "Dummy data inserted successfully!";
+
+
+
+
+
+
+
+
+    // INSERT A COUPLE OF BOOKS
+    if ( $mysqli->query("INSERT INTO books (name,author,description,category) VALUES ('Intro to C++','Stoustrup','Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus totam quasi, sit cumque unde ab minus numquam nostrum commodi doloribus!','Programming')") ) {
+        echo "Dummy book inserted successfully..\n";
     } else {
-        echo "Dummy data failed..";
+        echo "Dummy data failed..\n";
     }
-    if ( $mysqli->query("INSERT INTO books (name,author) VALUES ('EAM2','Rossu')") ) {
-        echo "Dummy data inserted successfully!";
+    if ( $mysqli->query("INSERT INTO books (name,author,description,category) VALUES ('Java for dummies','John Doe','Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus totam quasi, sit cumque unde ab minus numquam nostrum commodi doloribus!','Programming')") ) {
+        echo "Dummy book inserted successfully..\n";
     } else {
-        echo "Dummy data failed..";
+        echo "Dummy data failed..\n";
     }
-    if ( $mysqli->query("INSERT INTO books (name,author) VALUES ('EAM3','ussou')") ) {
-        echo "Dummy data inserted successfully!";
+    if ( $mysqli->query("INSERT INTO books (name,author,description,category) VALUES ('Basic Accounting','Nick Doe','Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus totam quasi, sit cumque unde ab minus numquam nostrum commodi doloribus!','Economics')") ) {
+        echo "Dummy book inserted successfully..\n";
     } else {
-        echo "Dummy data failed..";
+        echo "Dummy data failed..\n";
     }
-    if ( $mysqli->query("INSERT INTO books (name,author) VALUES ('EAM4','Rosou')") ) {
-        echo "Dummy data inserted successfully!";
+    if ( $mysqli->query("INSERT INTO books (name,author,description,category) VALUES ('Intro to C++','Stoustrup','Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus totam quasi, sit cumque unde ab minus numquam nostrum commodi doloribus!','Programming')") ) {
+        echo "Dummy book inserted successfully..\n";
     } else {
-        echo "Dummy data failed..";
+        echo "Dummy data failed..\n";
     }
-    if ( $mysqli->query("INSERT INTO books (name,author) VALUES ('EAM5','Rous')") ) {
-        echo "Dummy data inserted successfully!";
+    if ( $mysqli->query("INSERT INTO books (name,author,description,category) VALUES ('Intro to C++','Stoustrup','Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus totam quasi, sit cumque unde ab minus numquam nostrum commodi doloribus!','Programming')") ) {
+        echo "Dummy book inserted successfully..\n";
     } else {
-        echo "Dummy data failed..";
+        echo "Dummy data failed..\n";
+    }
+    if ( $mysqli->query("INSERT INTO books (name,author,description,category) VALUES ('Intro to C++','Stoustrup','Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus totam quasi, sit cumque unde ab minus numquam nostrum commodi doloribus!','Programming')") ) {
+        echo "Dummy book inserted successfully..\n";
+    } else {
+        echo "Dummy data failed..\n";
+    }
+    if ( $mysqli->query("INSERT INTO books (name,author,description,category) VALUES ('Intro to C++','Stoustrup','Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus totam quasi, sit cumque unde ab minus numquam nostrum commodi doloribus!','Programming')") ) {
+        echo "Dummy book inserted successfully..\n";
+    } else {
+        echo "Dummy data failed..\n";
+    }
+    if ( $mysqli->query("INSERT INTO books (name,author,description,category) VALUES ('Intro to C++','Stoustrup','Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus totam quasi, sit cumque unde ab minus numquam nostrum commodi doloribus!','Programming')") ) {
+        echo "Dummy book inserted successfully..\n";
+    } else {
+        echo "Dummy data failed..\n";
+    }
+    if ( $mysqli->query("INSERT INTO books (name,author,description,category) VALUES ('Intro to C++','Stoustrup','Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus totam quasi, sit cumque unde ab minus numquam nostrum commodi doloribus!','Programming')") ) {
+        echo "Dummy book inserted successfully..\n";
+    } else {
+        echo "Dummy data failed..\n";
+    }
+    
+
+    // INSERT A COUPLE ANNOUNCEMENTS
+    if( $mysqli->query("INSERT INTO announcements (title,text,img) VALUES ('Αλλαγή Πλατφόρμας','Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium architecto beatae, molestiae deserunt distinctio alias tempore obcaecati repellendus deleniti animi vel quam cupiditate, harum aliquid ipsa nemo voluptates quidem rerum explicabo optio facilis voluptatem mollitia repudiandae veniam! Ipsam, rerum voluptates!','img/ann/1.jpeg')") ) {
+        echo "Dummy announcement inserted successfully..\n";
+    } else {
+        echo "Dummy announcement failed..\n";
+    }
+    if( $mysqli->query("INSERT INTO announcements (title,text,img) VALUES ('Έναρξη Δηλώσεων','Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium architecto beatae, molestiae deserunt distinctio alias tempore obcaecati repellendus deleniti animi vel quam cupiditate, harum aliquid ipsa nemo voluptates quidem rerum explicabo optio facilis voluptatem mollitia repudiandae veniam! Ipsam, rerum voluptates!','img/ann/2.jpeg')") ) {
+        echo "Dummy announcement inserted successfully..\n";
+    } else {
+        echo "Dummy announcement failed..\n";
+    }
+    if( $mysqli->query("INSERT INTO announcements (title,text,img) VALUES ('Λήξη Δηλώσεων','Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium architecto beatae, molestiae deserunt distinctio alias tempore obcaecati repellendus deleniti animi vel quam cupiditate, harum aliquid ipsa nemo voluptates quidem rerum explicabo optio facilis voluptatem mollitia repudiandae veniam! Ipsam, rerum voluptates!','img/ann/3.jpeg')") ) {
+        echo "Dummy announcement inserted successfully..\n";
+    } else {
+        echo "Dummy announcement failed..\n";
+    }
+    if( $mysqli->query("INSERT INTO announcements (title,text,img) VALUES ('Παράταση στην Δήλωση Συγγραμάτων','Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium architecto beatae, molestiae deserunt distinctio alias tempore obcaecati repellendus deleniti animi vel quam cupiditate, harum aliquid ipsa nemo voluptates quidem rerum explicabo optio facilis voluptatem mollitia repudiandae veniam! Ipsam, rerum voluptates!','img/ann/4.jpeg')") ) {
+        echo "Dummy announcement inserted successfully..\n";
+    } else {
+        echo "Dummy announcement failed..\n";
     }
 ?> 
